30f974cbf3a67c344ac5711558a67928950a785e
ismaeeelxd <ismailmuhammed2026@gmail.com>
2023-12-04 18:20:14 +0200

    added User class

---
+++ src/coworking_space/User.java
@@ -0,0 +1,26 @@
+package coworking_space;
+
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected int ID;
+
+    public User(String username,String password){
+        this.username = username;
+        this.password = password;
+    }
+    public User(){
+
+    }
+
+    public abstract void Register();
+    public static String Login(String password, String username){
+        if(username.equals("admin") && password.equals("admin")){
+            return "admin";
+        }
+
+    }
+
+
+
+}
