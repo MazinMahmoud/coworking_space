e403ed66e275260387e89340fee3721870ed141f
alhasan88 <alhasanmohamedcs308@gmail.com>
2023-12-11 22:21:52 +0200

    Slot Updated

--- src/coworking_space/Slot.java
+++ src/coworking_space/Slot.java
@@ -6,21 +6,49 @@ import java.time.LocalTime;
 import java.util.Arrays;
 
 public class Slot {
-    private LocalTime time;
+    private LocalTime startTime;
+    private LocalTime endTime;
     private LocalDate date;
     private double fees;
-    //public static int counter;
-    //private static int array_of_slots[][]= new int [8][25];
-    //private static String dayName;
     private boolean isReserved;
-    public Slot(LocalTime time, LocalDate date, double fees, boolean isReserved) {
-        this.time = time;
+    private String roomID;
+    public Slot(LocalTime startTime, LocalTime endTime, LocalDate date, double fees, boolean isReserved,String roomID) {
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.date = date;
         this.fees = fees;
         this.isReserved = false;
-        //counter=0;
-
+        this.roomID = roomID;
    }
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getFees() {
+        return fees;
+    }
     public void reserve() {
         if (!this.isReserved) {
             this.isReserved = true;
@@ -29,7 +57,6 @@ public class Slot {
             System.out.println("Slot is already reserved.");
         }
     }
-
     public void cancelReservation() {
         if (this.isReserved) {
             this.isReserved = false;
@@ -38,78 +65,17 @@ public class Slot {
             System.out.println("There is no reservation to cancel");
         }
     }
+    public void updateSlot(LocalTime newStartTime, LocalTime newEndTime, LocalDate newDate, double newFees) {
+        if (!isReserved) {
+            setStartTime(newStartTime);
+            setEndTime(newEndTime);
+            setFees(newFees);
+            setDate(newDate);
+            System.out.println("Slot updated");
 
-    public void updateSlot(LocalTime newTime, LocalDate newDate, double newFees) {
-        if (newTime != null) {
-            this.time = newTime;
+        } else {
+
+            System.out.println("Slot update failed");
         }
-        if (newDate != null) {
-            this.date = newDate;
-        }
-        if (newFees >= 0) {
-            this.fees = newFees;
-        }
-        System.out.println("Slot updated");
     }
-    /*public static void reserve_aslot(int day,int from_h,int to_h){
-        for (int i=from_h;i<=to_h;i++){
-
-            array_of_slots[day][i]=counter;
-
-
-        }
-        counter++;
-    }
-    public static void display_slots(int day,int from_h,int to_h) {
-        switch (day) {
-            case 1:
-                dayName = "Saturday";
-                break;
-            case 2:
-                dayName = "Sunday";
-                break;
-            case 3:
-                dayName = "Monday";
-                break;
-            case 4:
-                dayName = "Tuesday";
-            case 5:
-                dayName = "Wednesday";
-                break;
-            case 6:
-                dayName = "Thursday";
-                break;
-            case 7:
-                dayName = "Friday";
-                break;
-        }
-        System.out.println("we have on" + dayName);
-        for (int i = from_h; i <= to_h; i++) {
-
-            if (array_of_slots[day][i] <20) {
-
-                System.out.println("in " + i + ":00" +array_of_slots[day][i]+"person");
-
-
-            }
-
-        }
-    }*/
-        public LocalTime getTime() {
-        return time;
-    }
-
-        public LocalDate getDate() {
-        return date;
-    }
-
-        public double getFees() {
-        return fees;
-    }
-    }
-    interface Reservable {
-        boolean reserveSlot(Slot slot, Vistor visitor);
-        boolean cancelReservation(Slot slot, Vistor visitor);
-        boolean updateReservation(Slot oldSlot, Slot newSlot, Vistor visitor);
-        void displayAvailableSlots();
     }
